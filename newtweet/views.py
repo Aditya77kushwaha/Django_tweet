49135b81277408e75ad7bf12e99fffc7c4793a1f
aditya77kushwaha <adityakushwaha2912@gmail.com>
2021-05-19 14:10:08 +0530

    Dynamic urls

--- newtweet/views.py
+++ newtweet/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
-def home(req):
-    return render(req, "home.html", {})
+def home(request):
+    return render(request, "home.html", {})
+
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    print(args, kwargs)
+    return HttpResponse(f"<h1>Hello {tweet_id}</h1>")
